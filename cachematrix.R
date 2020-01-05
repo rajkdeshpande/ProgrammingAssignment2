d3dc30e5ae31559428c4da7e3869eb109ff492f7
rajkdeshpande <59342453+rajkdeshpande@users.noreply.github.com>
2020-01-04 23:53:05 -0600

    Week3 Programming matrix inverse cache program

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y       # set in global environment
+    inv <<- NULL  # set in global environment
+  }
+  get <- function() { 
+    x
+  }
+  setInverse <- function() {
+    inv <<- solve(x) #calculate the inverse  # global evinronment
+  }
+  getInverse <- function() {
+    inv
+  }
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
